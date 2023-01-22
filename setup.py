bdad38354ad96c0e8432e55b268506abfa8870c7
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-01-22 14:28:35 -0500

    Fallback to yfinance if encountering API error with pandas-datareader.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='stock_analysis',
-    version='0.2',
+    version='0.2.1',
     description='Classes for technical analysis of stocks.',
     author='Stefanie Molin',
     author_email='24376333+stefmolin@users.noreply.github.com',
@@ -11,11 +11,12 @@ setup(
     packages=['stock_analysis'],
     install_requires=[
         'matplotlib>=3.0.2',
+        'mplfinance>=0.12.7a4',
         'numpy>=1.15.2',
         'pandas>=0.23.4',
         'pandas-datareader>=0.7.0',
         'seaborn>=0.11.0',
         'statsmodels>=0.11.1',
-        'mplfinance>=0.12.7a4'
+        'yfinance>=0.2.4'
     ],
 )
